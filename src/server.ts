0d6bdf72f4ad7b1c9b2cc67976322fcc798b0331
Danyyl <danyyl.con@gmail.com>
2022-01-22 13:59:37 +0200

    setup basic db connection

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import dotenv from 'dotenv';
 
 const app: express.Application = express()
-const address: string = "127.0.0.1:3000"
+const port = process.env.PORT
+const address: string = `Starting on port ${process.env.PORT}`
+
 
 app.use(bodyParser.json())
 
@@ -10,6 +13,6 @@ app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
 })
 
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log(`starting app on: ${address}`)
 })
